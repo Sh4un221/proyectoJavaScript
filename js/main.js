b86d58e8acc1d8e4ed05f992d27f27681f4d5177
Sh4un221 <127805282+Sh4un221@users.noreply.github.com>
2024-05-10 17:53:18 -0500

    feat: :construction: The project is still in progress.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-class myframe extends HTMLElement{
-    id
-    constructor(id){
+class MyFrame extends HTMLElement {
+    constructor() {
         super();
-        this.attachShadow({mode: "open"});
+        this.attachShadow({ mode: "open" });
     }
-    connectedCallback(){
-        this.shadowRoot.innerHTML = /*html*/`
-            <iframe class="spotify-iframe" width="100%" height="99%" src="https://open.spotify.com/embed/album/${this.id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        `
+    connectedCallback() {
+        if (this.getAttribute("uri")) {
+            const uriParts = this.getAttribute("uri").split(":");
+            const albumId = uriParts[2];
+            this.shadowRoot.innerHTML = `
+                <iframe class="spotify-iframe" width="100%" height="99%" src="https://open.spotify.com/embed/album/${albumId}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            `;
+        }
     }
-    static get observedAttributes(){
+    static get observedAttributes() {
         return ["uri"];
     }
-    attributeChangedCallback(name,old,now){
-        let[nameUri, album, id] = now.split(":")
-        this.id = id;
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "uri" && oldValue !== newValue) {
+            const uriParts = newValue.split(":");
+            const albumId = uriParts[2];
+            this.shadowRoot.innerHTML = `
+                <iframe class="spotify-iframe" width="100%" height="99%" src="https://open.spotify.com/embed/album/${albumId}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            `;
+        }
     }
 }
-customElements.define("my-frame",myframe)
+customElements.define("my-frame", MyFrame);
